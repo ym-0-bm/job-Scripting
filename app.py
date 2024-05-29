4773de5340d99b01a2841b72d505a3290ca08834
Ym-0-bm <93938113+ym-0-bm@users.noreply.github.com>
2024-05-29 11:21:12 +0000

    new docker build

--- app.py
+++ app.py
@@ -8,11 +8,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'clés_flash'
 conn = pymysql.connect(
     host=app.config['localhost'],
-    user=app.config['root'],
-    password=app.config[''],
-    db=app.config['job-scrapingdb'],
+    user=app.config['user'],
+    password=app.config['password'],
+    db=app.config['job-scraper'],
     cursorclass=pymysql.cursors.DictCursor
 )
+
+
+# Initialisation de la login MySQL
+def get_db_connection():
+    conn = pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB'],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return conn
 
 
 @app.route("/signin", methods=["GET", "POST"])
@@ -22,7 +34,7 @@ def signin():
         email = request.form["mail"]
         password = request.form["password"]
         hashed_password = generate_password_hash(password)
-        conn = pyodbc.connect(DSN)
+        conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM Users WHERE NomUser = ? OR Email = ?', (user, email))
         users = cursor.fetchall()
@@ -30,12 +42,12 @@ def signin():
             flash("ce compte existe déjà !", 'info')
         elif not re.match(r'[a-zA-Z0-9]+$', user):
             flash("Le nom d'utilisateur ne doit contenir que des lettres et des chiffres !", 'info')
-            return redirect(url_for('inscription'))
+            return redirect(url_for('signin'))
         elif not re.match(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
             flash("Email Invalid !", 'info')
-            return redirect(url_for('inscription'))
+            return redirect(url_for('signin'))
         else:
-            conn = pyodbc.connect(DSN)
+            conn = get_db_connection()
             cursor = conn.cursor()
             cursor.execute('''
                 INSERT INTO Users (NomUser, Email, Password)
@@ -44,8 +56,8 @@ def signin():
             conn.commit()
             conn.close()
             flash("Votre compte a été enregistré avec succès !", 'info')
-            return redirect(url_for('connexion'))
-    return render_template("/connexion/inscription.html")
+            return redirect(url_for('login'))
+    return render_template("/registration/signin.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -53,7 +65,7 @@ def login():
     if request.method == 'POST':
         user = request.form["identifiant"]
         password = request.form["password"]
-        conn = pyodbc.connect(DSN)
+        conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM Users WHERE NomUser = ? OR Email = ?', (user, user))
         users = cursor.fetchone()
@@ -66,18 +78,18 @@ def login():
                 return redirect(url_for('accueil'))
             else:
                 flash("Mot de passe incorrect !", 'info')
-                return redirect(url_for('connexion'))
+                return redirect(url_for('login'))
         else:
             flash("Identifiant incorrect !", 'info')
-            return redirect(url_for('connexion'))
-    return render_template("/connexion/connexion.html")
+            return redirect(url_for('login'))
+    return render_template("/registration/login.html")
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if 'loggedin' in session:
-        return render_template("/connexion/accueil.html", username=session['username'], title="accueil")
-    return redirect(url_for('connexion'))
+        return render_template("/login/accueil.html", username=session['username'], title="accueil")
+    return redirect(url_for('login'))
 
 
 @app.route("/logout")
@@ -85,8 +97,8 @@ def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
-    return redirect(url_for('connexion'))
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
